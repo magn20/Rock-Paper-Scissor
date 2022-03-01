b8f49d57ced94eda1d8bf18485d3338429fd2877
Math376i <91528384+Math376i@users.noreply.github.com>
2022-03-01 12:58:05 +0100

    .

---
+++ src/rps/bll/player/AiPlayer.java
@@ -0,0 +1,23 @@
+package rps.bll.player;
+
+import rps.bll.game.IGameState;
+import rps.bll.game.Move;
+
+public class AiPlayer implements IPlayer{
+
+
+    @Override
+    public String getPlayerName() {
+        return null;
+    }
+
+    @Override
+    public PlayerType getPlayerType() {
+        return null;
+    }
+
+    @Override
+    public Move doMove(IGameState state) {
+        return null;
+    }
+}
