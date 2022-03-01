3e4cd03a6ab5a2f25042bf3061a055081e8d5575
AndersJakobsen <ande12n3@easv365.dk>
2022-03-01 10:53:07 +0100

    .
    Opens Gui

--- src/rps/Main.java
+++ src/rps/Main.java
@@ -1,19 +1,24 @@
 package rps;
 
 //Project imports
+import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import rps.gui.ConsoleApp;
 import rps.gui.JavaFXApp;
+import javafx.scene.Scene;
+
+// import static javafx.application.Application.launch;
 
 /**
  * Main class where we start
  *
  * @author smsj
  */
-public class Main {
-// helllo
+public class Main extends Application {
+
+    private static FXMLLoader fxmlLoaderMain;
 
     /**
      * Main start
@@ -21,29 +26,36 @@ public class Main {
      */
     public static void main(String[] args)
     {
+        launch(args);
         //Console version
-        startRPSConsoleGame();
-
+        
+        //startRPSConsoleGame();
         //JavaFX version
         //startRPSJavaFXGame();
     }
 
-    public void start(Stage primaryStage) throws Exception
-    {
-        Parent pane = FXMLLoader.load(
-                getClass().getResource( "GameView.fxml" ) );
+
+    public void start(Stage primaryStage) throws Exception {
+        fxmlLoaderMain = new FXMLLoader(getClass().getResource("./gui/view/GameView.fxml"));
+        primaryStage.centerOnScreen();
+        Scene scene = new Scene(fxmlLoaderMain.load());
+        primaryStage.setResizable(false);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Rock, Paper, Scissor");
+        primaryStage.show();
     }
+
     /**
      * Start a JavaFX version of the game
      */
-    private static void startRPSJavaFXGame() {
-        JavaFXApp.launch();
+    private void startRPSJavaFXGame() {
+         Application.launch();
     }
 
     /**
      * Start a console version of the game
      */
-    public static void startRPSConsoleGame() {
+    public void startRPSConsoleGame() {
         new ConsoleApp().startGame();
     }
 }
